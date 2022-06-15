834e484aeb02888523c1430bdb02f5e81f03e366
Mohammadreza Fakhraei <mzf0069@auburn.edu>
2022-06-15 14:37:06 -0500

    Add index grid class

---
+++ smolyay/smolyak.py
@@ -0,0 +1,5 @@
+class IndexGrid:
+    def __init__(self, dimension, exactness, index_per_level):
+        self.dimension = dimension
+        self.exactness = exactness
+        self.index_per_level = index_per_level
