324db2f559bd75f28ba135e773212a0b7f77a09a
mzbush <mzb0108@auburn.edu>
2022-07-29 16:12:32 -0500

    Write expected points in exact mathematical terms

--- tests/basis_test.py
+++ tests/basis_test.py
@@ -1,26 +1,21 @@
 import pytest
+
+
+import numpy
+
 from smolyay.basis.basis import BasisFunction, ChebyshevFirstKind
 
 @pytest.fixture
 def expected_extrema_2():
     """extrema for exactness = 2"""
-    return [0, -1.0, 1.0, -0.707106781186548, 0.707106781186548]
+    return [0, -1.0, 1.0, -1/(2**0.5), 1/(2**0.5)]
 
 @pytest.fixture
 def expected_extrema_3():
     """extrema for exactness = 3"""
-    return [0, -1.0, 1.0, -0.707106781186548, 0.707106781186548, 
-            -0.923879532511287, -0.38268343236509, 0.38268343236509, 
-            0.923879532511287]
-
-@pytest.fixture
-def expected_extrema_4():
-    """extrema for exactness = 4"""
-    return [0, -1.0, 1.0, -0.707106781186548, 0.707106781186548,
-            -0.923879532511287, -0.38268343236509, 0.38268343236509, 
-            0.923879532511287, -0.98078528040323, -0.831469612302545, 
-            -0.555570233019602, -0.195090322016128, 0.195090322016128, 
-            0.555570233019602, 0.831469612302545, 0.98078528040323]
+    return [0, -1.0, 1.0, -1/(2**0.5), 1/(2**0.5), 
+            -(((2**0.5)+1)**0.5)/(2**0.75),-(((2**0.5)-1)**0.5)/(2**0.75),
+            (((2**0.5)-1)**0.5)/(2**0.75),(((2**0.5)+1)**0.5)/(2**0.75)]
 
 def test_exactness_zero():
     """test exactness of zero"""
@@ -40,25 +35,33 @@ def test_initial_exactness_2(expected_extrema_2):
     """test initial when exactness is 2"""
     test_class = ChebyshevFirstKind(2)
     assert test_class.max_exactness == 2
-    assert test_class.points == expected_extrema_2
+    assert numpy.allclose(test_class.points,expected_extrema_2,atol=1e-10)
     assert test_class.levels == [[0],[1,2],[3,4]]
 
-def test_increase_exactness(expected_extrema_4):
+def test_increase_exactness(expected_extrema_3):
     """test when max exactness is increased"""
-    test_class = ChebyshevFirstKind(2)
-    test_class.max_exactness = 4
-    assert test_class.max_exactness == 4
-    assert test_class.points == expected_extrema_4
-    assert test_class.levels == [[0],[1,2],[3,4],[5,6,7,8],
-            [9,10,11,12,13,14,15,16]]
+    test_class = ChebyshevFirstKind(1)
+    test_class.max_exactness = 3
+    assert test_class.max_exactness == 3
+    assert numpy.allclose(test_class.points, expected_extrema_3,atol=1e-10)
+    assert test_class.levels == [[0],[1,2],[3,4],[5,6,7,8]]
 
 def test_decrease_exactness(expected_extrema_3):
     """test when max exactness is decreased"""
     test_class = ChebyshevFirstKind(5)
     test_class.max_exactness = 3
     assert test_class.max_exactness == 3
-    assert test_class.points == expected_extrema_3
+    assert numpy.allclose(test_class.points,expected_extrema_3,atol=1e-10)
     assert test_class.levels == [[0],[1,2],[3,4],[5,6,7,8]]
+
+def test_decrease_increase_exactness(expected_extrema_2):
+    """test when max exactness is decreased"""
+    test_class = ChebyshevFirstKind(1)
+    test_class.max_exactness = 6
+    test_class.max_exactness = 2
+    assert test_class.max_exactness == 2
+    assert numpy.allclose(test_class.points,expected_extrema_2,atol=1e-10)
+    assert test_class.levels == [[0],[1,2],[3,4]]
 
 def test_basis_degree_0():
     """Chebyshev polynomial degree 0 is 1"""
