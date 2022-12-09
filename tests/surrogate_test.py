e7f2547f04f4698b3b330015724f68fd68a30de6
Mohammadreza <107329314+mzf0069@users.noreply.github.com>
2022-12-09 13:05:29 -0600

    Feature/surrogate1 (#4)
    * Create class Surrogate
    
    * Change variable names.
    
    * Fix issue with train_from_data.
    
    * Fix issue with 1D functions.
    
    * Add lstsq method.
    
    * Make solve function.
    
    * Modify the documentation.
    
    * Implement reviewer's suggestions.
    
    * Updating documentation.
    
    * Implement suggestions made by reviewer.
    
    * Fix typing for surrogate arguments, map function, and 1D
    
    * Simplify surrogate unit tests.
    
    * Implement reviewer's suggestions.
    
    * Update documentation.
    
    * Implement suggestions made by reviewer.
    
    * Modify .
    
    Co-authored-by: Michael Howard <mphoward@auburn.edu>

---
+++ tests/surrogate_test.py
@@ -0,0 +1,197 @@
+import numpy
+import pytest
+
+from smolyay.basis import ChebyshevFirstKind
+from smolyay.grid import SmolyakGridGenerator, IndexGrid
+from smolyay.surrogate import Surrogate
+
+
+def function_0(x):
+    """Test function 0."""
+    x1, x2, x3 = x
+    return 2*x1 + x2 - x3
+
+
+def function_0_shifted(x):
+    """Test fucntion 0 which is shifted."""
+    x1, x2, x3 = x
+    return 2*(x1 - 1) + (x2 + 1) - x3
+
+
+def function_1(x):
+    """Test fucntion 1."""
+    x1, x2 = x
+    return x1 + (2*x2**2 - 1)
+
+
+def function_2(x):
+    """Test funciton 2."""
+    return x**2 - 3 * (2 + x) - x
+
+
+def branin(x):
+    """Branin function."""
+    x1, x2 = x
+    branin1 = (x2 - 5.1 * x1 ** (2)/(4 * numpy.pi ** 2)
+               + 5 * x1 / (numpy.pi) - 6) ** 2
+    branin2 = 10 * (1 - 1 / (8 * numpy.pi)) * numpy.cos(x1)
+    branin3 = 10
+    branin_function = branin1 + branin2 + branin3
+    return branin_function
+
+
+def test_initialization_1d():
+    """Test if class is properly intiallized."""
+    grid_generator = SmolyakGridGenerator(
+        ChebyshevFirstKind.make_nested_set(2))
+    domain = (-1, 1)
+    surrogate = Surrogate(domain, grid_generator)
+    assert numpy.allclose(surrogate.domain, domain)
+    assert surrogate.coefficients is None
+    assert isinstance(surrogate.grid, IndexGrid)
+    assert surrogate.dimension == 1
+    assert numpy.allclose(surrogate.points,
+                          [0, -1, 1, -0.70710, 0.70710])
+
+
+def test_initialization_2d():
+    """Test if class is properly intiallized."""
+    grid_generator = SmolyakGridGenerator(
+        ChebyshevFirstKind.make_nested_set(2))
+    domain = [(-5, 5), (-8, 0)]
+    surrogate = Surrogate(domain, grid_generator)
+    grid_object = surrogate.grid
+    assert numpy.allclose(surrogate.domain,
+                          domain)
+    assert surrogate.coefficients is None
+    assert isinstance(grid_object, IndexGrid)
+    assert surrogate.dimension == 2
+    assert numpy.allclose(surrogate.points, [[0, -4], [-5, -4], [5, -4],
+                                             [0, -8], [0, 0], [-3.5355, -4],
+                                             [3.5355, -4], [-5, -8],
+                                             [-5, 0], [5, -8], [5, 0],
+                                             [0, -6.8284], [0, -1.1715]],
+                          atol=1e-4)
+
+
+def test_error_grid_generator_type():
+    """Test grid generator's type."""
+    domain = [(-5, 5), (-8, 0)]
+    with pytest.raises(TypeError):
+        Surrogate(domain, 'not a grid generator')
+
+
+def test_map_function_1d():
+    """Test if points are transfromed properly."""
+    grid = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(1))
+    domain = (-8, 12)
+    new_domain = (0, 1)
+    f = Surrogate(domain, grid)
+    assert f._mapdomain(2, domain, new_domain) == pytest.approx(0.5)
+    assert numpy.allclose(f._mapdomain([-3, 7],
+                                       domain, new_domain), [0.25, 0.75])
+
+
+def test_map_function_2d():
+    """Test if points are transfromed properly."""
+    grid = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(1))
+    domain = ((-10, 10), (0, 2))
+    new_domain = ((0, 1), (-1, 1))
+    f = Surrogate(domain, grid)
+    assert numpy.allclose(f._mapdomain((-10, 0), domain, new_domain), [0, -1])
+    assert numpy.allclose(f._mapdomain([(0, 1), (10, 2)],
+                                       domain, new_domain), [[0.5, 0], [1, 1]])
+
+
+@pytest.mark.parametrize('linear_solver', ['lu', 'lstsq', 'inv'])
+def test_surrogate_0(linear_solver):
+    """Test if surrogate generates exact results for test fucntion 0."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate = Surrogate([(-1, 1), (-1, 1), (-1, 1)], grid_gen)
+    surrogate.train(function_0, linear_solver)
+    # random points in the domain
+    points = [(0.649, 0, -0.9), (-0.885, 1, 0.275)]
+    surrogate_values = [surrogate(x) for x in points]
+    exact_values = [function_0(x) for x in points]
+    assert numpy.allclose(surrogate_values, exact_values)
+
+
+def test_surrogate_1():
+    """Test if surrogate generates exact results for test for Chebyshevs."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate = Surrogate([(-1, 1), (-1, 1)], grid_gen)
+    surrogate.train(function_1)
+    # random points in the domain
+    points = [(0.649, -0.9), (-0.885, 1)]
+    surrogate_values = [surrogate(x) for x in points]
+    exact_values = [function_1(x) for x in points]
+    assert numpy.allclose(surrogate_values, exact_values)
+
+
+def test_surrogate_0_shifted():
+    """Test if surrogate generates exact results for a shifted function 0."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate_1 = Surrogate([(-1, 1), (-1, 1), (-1, 1)], grid_gen)
+    surrogate_2 = Surrogate([(0, 2), (-2, 0), (-1, 1)], grid_gen)
+    surrogate_1.train(function_0)
+    surrogate_2.train_from_data(surrogate_1.data)
+    point, point_shifted = (0, 0.5, -0.5), (1, -0.5, -0.5)
+    assert numpy.allclose(surrogate_1.coefficients,
+                          surrogate_2.coefficients)
+    assert numpy.isclose(surrogate_1(point), surrogate_2(point_shifted))
+
+
+@pytest.mark.parametrize('linear_solver', ['lu', 'lstsq', 'inv'])
+def test_surrogate_2(linear_solver):
+    """Test if surrogate generates exact results for simple 1D function."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate = Surrogate((-10, 10), grid_gen)
+    surrogate.train(function_2, linear_solver)
+    # random point in the domain
+    point = 0.367
+    assert numpy.isclose(surrogate(point), function_2(point))
+
+
+def test_train_from_data():
+    """Test if train_from_data_works."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(4))
+    surrogate = Surrogate([(-5, 10), (0, 15)], grid_gen)
+    data = [branin(point) for point in surrogate.points]
+    surrogate.train_from_data(data)
+    # random point in the domain
+    point = (8, 0.75)
+    assert numpy.isclose(surrogate(point), branin(point))
+    with pytest.raises(IndexError):
+        surrogate.train_from_data(data[:5])
+
+
+def test_error_solver():
+    """Test if invalid solver generates surrogate."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate = Surrogate((-10, 10), grid_gen)
+    with pytest.raises(ValueError):
+        surrogate.train(function_2, 'inverse')
+
+
+def test_error_function_needs_training():
+    """Test if surrogate is generated without training."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate = Surrogate([(-1, 1), (-1, 1)], grid_gen)
+    # random point in the domain
+    point = (0.649, 0)
+    with pytest.raises(ValueError):
+        surrogate(point)
+
+
+def test_error_input_surrogate():
+    """Test the error related to surrogate's input dimension."""
+    grid_gen = SmolyakGridGenerator(ChebyshevFirstKind.make_nested_set(2))
+    surrogate = Surrogate([(-1, 1), (-1, 1)], grid_gen)
+    surrogate.train(function_1, 'lstsq')
+    # random point in the domain
+    point = (0.649, 0, 0.5)
+    point_outside_domain = (-2, 1)
+    with pytest.raises(IndexError):
+        surrogate(point)
+    with pytest.raises(ValueError):
+        surrogate(point_outside_domain)
