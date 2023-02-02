3d89b84661bcc4c23da90be4d6de6d73b039cf30
mzbush <mzb0108@auburn.edu>
2023-02-02 11:52:32 -0600

    Add new test for empty element in middle of levels

--- tests/slow_growth_test.py
+++ tests/slow_growth_test.py
@@ -13,3 +13,15 @@ def test_slow_growth():
     grid = grid_gen(2)
     assert grid.indexes == [[0, 0],[1, 0],[2, 0],[0, 1],[0, 2],[1, 1],[1, 2],
             [2, 1],[2, 2]]
+
+def test_slow_growth_2():
+    """Test grid indexes if levels property is empty in middle"""
+    levels = [[0], [1,2], [], [3,4]]
+    points = [0, -1, 1, -1/(2**0.5), 1/(2**0.5)]
+    basis_functions = [ChebyshevFirstKind(n) for n in range(5)]
+    nest_set = NestedBasisFunctionSet(points,basis_functions,levels)
+    grid_gen = SmolyakGridGenerator(nest_set)
+    grid = grid_gen(2)
+    assert grid.indexes == [[0, 0],[1, 0],[2, 0],[0, 1],[0, 2],[1, 1],[1, 2],
+            [2, 1],[2, 2],[3, 0],[4, 0],[0, 3],[0, 4]]
+
