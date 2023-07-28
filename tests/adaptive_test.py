01f5133954d0a5a12224064689b00ab127fcec6e
mzbush <mzb0108@auburn.edu>
2023-07-28 12:30:06 -0500

    Add ability to enter custom precision rule

--- tests/adaptive_test.py
+++ tests/adaptive_test.py
@@ -29,7 +29,7 @@ def test_initial_three(expected_points_3_set):
     f = make_slow_nested_set(3)
     assert numpy.allclose(
             f.points,expected_points_3_set,atol=1e-10)
-    assert f.levels == [[0],[1,2],[3,4],[5,6,7,8]]
+    assert f.levels == [[0], [1, 2], [3, 4], [5, 6, 7, 8]]
     basis_functions = f.basis_functions
     assert len(basis_functions) == 9
     for i in range(0,len(basis_functions)):
@@ -40,8 +40,14 @@ def test_initial_four(expected_points_3_set):
     f = make_slow_nested_set(4)
     assert numpy.allclose(
             f.points,expected_points_3_set,atol=1e-10)
-    assert f.levels == [[0],[1,2],[3,4],[5,6,7,8],[]]
+    assert f.levels == [[0], [1, 2], [3, 4], [5, 6, 7, 8], []]
     basis_functions = f.basis_functions
     assert len(basis_functions) == 9
     for i in range(0,len(basis_functions)):
         assert basis_functions[i].n == i
+
+def test_custom_rule():
+    """Creates a slower NestedBasisFunctionSet"""
+    f = make_slow_nested_set(4, lambda x : x+1)
+    assert f.levels == [[0], [1, 2], [], [3, 4], []]
+    assert len(f.basis_functions) == 5
