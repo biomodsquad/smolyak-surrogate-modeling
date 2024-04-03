1e075b6dabdf7416b21ec867d81023a653ce22ad
mzbush <mzb0108@auburn.edu>
2024-04-03 18:11:37 -0500

    Add z-score normalizer

--- smolyay/normalize.py
+++ smolyay/normalize.py
@@ -1,7 +1,6 @@
 import abc
 
 import numpy
-import scipy.special
 
 
 class Normalizer(abc.ABC):
@@ -10,19 +9,19 @@ class Normalizer(abc.ABC):
     Prior to training a surrogate model, a transformation can be applied
     to the data to put it on a different scale or otherwise normalize it.
     Transformed data can potentially result in a better behaved surrogate,
-    depending on the normalization and the surrogate. The output of the 
+    depending on the normalization and the surrogate. The output of the
     surrogate trained on normalized data must be unnormalized to match the
     real function.
 
-    This class specifies a type of normalization, defined by a 
-    transform function :meth:`transform` and an inverse transform 
+    This class specifies a type of normalization, defined by a
+    transform function :meth:`transform` and an inverse transform
     :meth:`inverse_transform`. :meth:`transform` normalizes the training
     data for the surrogate, and :meth:`inverse_transform` unnormalizes
-    the output of the surrogate. If defined correctly, applying 
+    the output of the surrogate. If defined correctly, applying
     :meth:`transform` and :meth:`inverse_transform` sequentially should
     return the initial data.
     ``original_data`` is the original, unnormalized training data used to
-    calibrate :meth:`transform` and :meth:`inverse_transform`, if 
+    calibrate :meth:`transform` and :meth:`inverse_transform`, if
     calibration is necessary.
     """
 
@@ -33,7 +32,7 @@ class Normalizer(abc.ABC):
     def original_data(self):
         """numpy.ndarray: training data before normalization."""
         return self._original_data
-    
+
     @original_data.setter
     def original_data(self, value):
         value = numpy.array(value, ndmin=1)
@@ -41,14 +40,14 @@ class Normalizer(abc.ABC):
         self._training_data = self.transform(self.original_data)
 
     @abc.abstractmethod
-    def transform(self,x):
+    def transform(self, x):
         """Normalization function
 
         Parameters
         ----------
         x : numerical data
             data to be transformed
-        
+
         Return
         ------
         normalized data
@@ -56,38 +55,38 @@ class Normalizer(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def inverse_transform(self,x):
+    def inverse_transform(self, x):
         """Inverse normalization function
 
         Parameters
         ----------
         x : numerical data
             normalized data to be transformed
-        
+
         Return
         ------
         unnormalized data
         """
         pass
 
-    def check_normalize_error(self,x,percent_diff=False):
+    def check_normalize_error(self, x, percent_diff=False):
         """Check error from normalizing process
-        If defined correctly, performing :meth:`inverse_transform` on 
-        the output of :meth:`transform` should return the input of 
+        If defined correctly, performing :meth:`inverse_transform` on
+        the output of :meth:`transform` should return the input of
         :meth:`transform`. As theory does not always align with practice,
-        this method checks how much the data changes from its initial 
-        value after the transform and inverse transform are done in 
-        sequence. This error is returned as the root mean squared error 
-        if `percent_changed` is false and as the percent difference from 
-        the initial stat if `percent_diff` is true. If the data is 
-        multiple values, the percent difference is equivalent to the 
+        this method checks how much the data changes from its initial
+        value after the transform and inverse transform are done in
+        sequence. This error is returned as the root mean squared error
+        if `percent_changed` is false and as the percent difference from
+        the initial stat if `percent_diff` is true. If the data is
+        multiple values, the percent difference is equivalent to the
         symmetrical mean absolute percentage error.
 
         Parameters
         ----------
         x : numerical data
-            data to compare before and after transformation 
-        
+            data to compare before and after transformation
+
         percent_diff : bool
             returns RMSE if false, symmape if true
 
@@ -100,54 +99,56 @@ class Normalizer(abc.ABC):
         new_x = self.inverse_transform(self.transform(x))
         if percent_diff:
             # if absolute difference is 0, then percent difference is 0.
-            abs_diff = numpy.abs(x-new_x)
-            abs_diff[abs_diff!=0] = abs_diff[abs_diff!=0]*2/(x + new_x)
+            abs_diff = numpy.abs(x - new_x)
+            abs_diff[abs_diff != 0] = abs_diff[abs_diff != 0] * 2 / (x + new_x)
             return numpy.mean(abs_diff)
         else:
-            return numpy.sqrt(numpy.mean((x - new_x)**2))
+            return numpy.sqrt(numpy.mean((x - new_x) ** 2))
+
 
 class NullNormalizer(Normalizer):
-    """"Normalizer that does not modify data
-    
+    """ "Normalizer that does not modify data
+
     Using this normalizer will not transform the data in any way
     and is equilivant to not doing any normalization or transformation.
     """
 
-    def transform(self,x):
+    def transform(self, x):
         """Normalization function
 
         Parameters
         ----------
         x : numerical data
             data to be transformed
-        
+
         Return
         ------
         normalized data
         """
         return x
 
-    def inverse_transform(self,x):
+    def inverse_transform(self, x):
         """Inverse normalization function
 
         Parameters
         ----------
         x : numerical data
             normalized data to be transformed
-        
+
         Return
         ------
         unnormalized data
         """
         return x
 
+
 class IntervalNormalizer(Normalizer):
     """Scales data onto the interval [0,1]
-    
-    Using the min and max of the original training data, the training 
-    data is normalized to the range [0,1]. 
-    
-    The tranformation equation is 
+
+    Using the min and max of the original training data, the training
+    data is normalized to the range [0,1].
+
+    The tranformation equation is
     ..math::
         y = (x - min_val/(max_val - min_val)
         where :math:min_val is the minimum of the training data and
@@ -159,9 +160,9 @@ class IntervalNormalizer(Normalizer):
     The inverse transformation equation is
     ..math::
         y = x * (max_val - min_val) + min_val
-    
+
     In the case where the original training data contains one variable,
-    and thus the min and the max are the same, no normalization is 
+    and thus the min and the max are the same, no normalization is
     applied.
 
     The properties ``min_val`` and ``max_val`` store the min and max of
@@ -172,12 +173,12 @@ class IntervalNormalizer(Normalizer):
         super().__init__()
         self._min_val = None
         self._max_val = None
-    
+
     @property
     def min_val(self):
         """float: min of original training data"""
         if self.original_data is None:
-            raise ValueError('The original data was never given')
+            raise ValueError("The original data was never given")
         if self._min_val is None:
             self._min_val = numpy.min(self.original_data)
         return self._min_val
@@ -186,16 +187,15 @@ class IntervalNormalizer(Normalizer):
     def max_val(self):
         """float: max of original training data"""
         if self.original_data is None:
-            raise ValueError('The original data was never given')
+            raise ValueError("The original data was never given")
         if self._max_val is None:
             self._max_val = numpy.max(self.original_data)
         return self._max_val
-    
-    
-    def transform(self,x):
+
+    def transform(self, x):
         """Normalize the data using the min and max
 
-        Using the min and max of the training data, the input is 
+        Using the min and max of the training data, the input is
         scaled such that the max of the original training data is 1 and
         the min of the original traning data is 0.
 
@@ -203,7 +203,7 @@ class IntervalNormalizer(Normalizer):
         ----------
         x : numerical data
             data to be transformed
-        
+
         Return
         ------
         normalized data
@@ -214,14 +214,13 @@ class IntervalNormalizer(Normalizer):
             the original data was never assigned
         """
         if self.original_data is None:
-            raise ValueError('The original data was never given')
+            raise ValueError("The original data was never given")
         if self.min_val >= self.max_val:
             return x
         else:
-            return (x - self.min_val)/(self.max_val - self.min_val)
+            return (x - self.min_val) / (self.max_val - self.min_val)
 
-
-    def inverse_transform(self,x):
+    def inverse_transform(self, x):
         """Inverse normalization function
 
         Using the min and max of the training data, the input is
@@ -232,7 +231,7 @@ class IntervalNormalizer(Normalizer):
         ----------
         x : numerical data
             normalized data to be transformed
-        
+
         Return
         ------
         unnormalized data
@@ -243,12 +242,117 @@ class IntervalNormalizer(Normalizer):
             the original data was never assigned
         """
         if self.original_data is None:
-            raise ValueError('The original data was never given')
+            raise ValueError("The original data was never given")
         if self.min_val >= self.max_val:
             return x
         else:
             return x * (self.max_val - self.min_val) + self.min_val
-        
+
+
+class ZScoreNormalizer(Normalizer):
+    """Normalizes data by setting the mean to 0 and std to 1
+
+    Using the mean and standard deviation of the original training data,
+    the training data is normalized so the mean becomes 0 and the standard
+    deviation becomes 1.
+
+    The tranformation equation is
+    ..math::
+        y = (x - mean_data) / std_data
+        where :math:mean_data is the mean of the training data and
+        :math:std_data is the standard deviation of the training data
+
+    The inverse transformation equation is
+    ..math::
+        y = x * std_data + mean_data
+
+    In the case where the original training data contains one variable,
+    and thus the min and the max are the same, no normalization is
+    applied.
+
+    The properties ``mean_val`` and ``std_val`` store the mean and
+    standard deviation of the original training data.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._mean_val = None
+        self._std_val = None
+
+    @property
+    def mean_val(self):
+        """float: mean of original training data"""
+        if self.original_data is None:
+            raise ValueError("The original data was never given")
+        if self._mean_val is None:
+            self._mean_val = numpy.mean(self.original_data)
+        return self._mean_val
+
+    @property
+    def std_val(self):
+        """float: std of original training data"""
+        if self.original_data is None:
+            raise ValueError("The original data was never given")
+        if self._std_val is None:
+            try:
+                self._std_val = numpy.std(
+                    numpy.array(self.original_data, dtype=numpy.float128)
+                ).astype(numpy.float64)
+            except AttributeError:
+                self._std_val = numpy.std(self.original_data)
+        return self._std_val
+
+    def transform(self, x):
+        """Normalize the data using the min and max
+
+        Using the min and max of the training data, the input is
+        scaled such that the max of the original training data is 1 and
+        the min of the original traning data is 0.
+
+        Parameters
+        ----------
+        x : numerical data
+            data to be transformed
+
+        Return
+        ------
+        normalized data
+
+        Raises
+        ------
+        ValueError
+            the original data was never assigned
+        """
+        if self.original_data is None:
+            raise ValueError("The original data was never given")
+        return (x - self.mean_val) / (self.std_val)
+
+    def inverse_transform(self, x):
+        """Inverse normalization function
+
+        Using the min and max of the training data, the input is
+        scaled such that 1 becomes the max of the original training data
+        and 0 becomes the min of the original training data
+
+        Parameters
+        ----------
+        x : numerical data
+            normalized data to be transformed
+
+        Return
+        ------
+        unnormalized data
+
+        Raises
+        ------
+        ValueError
+            the original data was never assigned
+        """
+        if self.original_data is None:
+            raise ValueError("The original data was never given")
+        return x * self.std_val + self.mean_val
+
+
 class SymmetricalLogNormalizer(Normalizer):
     r"""Transforms data onto the symmetrical logarithm scale
     
@@ -290,7 +394,7 @@ class SymmetricalLogNormalizer(Normalizer):
         y = \sgn(x) c (-1 + 10^{abs(x)})
     """
 
-    def __init__(self,linthresh=1):
+    def __init__(self, linthresh=1):
         super().__init__()
         self._linthresh = linthresh
 
@@ -298,38 +402,37 @@ class SymmetricalLogNormalizer(Normalizer):
     def linthresh(self):
         """constant to determine size of linear interval around 0"""
         return self._linthresh
-    
+
     @linthresh.setter
     def linthresh(self, value):
         if value <= 0:
             raise ValueError("linthresh must be greater than 0.")
         self._linthresh = value
 
-    def transform(self,x):
+    def transform(self, x):
         """Normalization function
 
         Parameters
         ----------
         x : numerical data
             data to be transformed
-        
+
         Return
         ------
         normalized data
         """
-        return numpy.sign(x)*numpy.log10(1+numpy.abs(x)/self.linthresh)
+        return numpy.sign(x) * numpy.log10(1 + numpy.abs(x) / self.linthresh)
 
-
-    def inverse_transform(self,x):
+    def inverse_transform(self, x):
         """Inverse normalization function
 
         Parameters
         ----------
         x : numerical data
             normalized data to be transformed
-        
+
         Return
         ------
         unnormalized data
         """
-        return numpy.sign(x)*self.linthresh*(-1 + numpy.power(10,numpy.abs(x)))
+        return numpy.sign(x) * self.linthresh * (-1 + numpy.power(10, numpy.abs(x)))
--- tests/normalize_test.py
+++ tests/normalize_test.py
@@ -4,7 +4,8 @@ import warnings
 import numpy
 
 from smolyay.normalize import (Normalizer,NullNormalizer,
-                               SymmetricalLogNormalizer,IntervalNormalizer)
+                               SymmetricalLogNormalizer,IntervalNormalizer,
+                               ZScoreNormalizer)
 
 
 def test_check_null():
@@ -34,8 +35,53 @@ def test_interval_min():
     normal.original_data = x
     assert normal.min_val == 1
 
+def test_interval_transform_error():
+    """Test if IntervalNormalizer returns an error without original data"""
+    normal = IntervalNormalizer()
+    with pytest.raises(ValueError):
+        normal.transform([1,2])
+    with pytest.raises(ValueError):
+        normal.inverse_transform([1,2]) 
+    with pytest.raises(ValueError):
+        normal.max_val
+    with pytest.raises(ValueError):
+        normal.min_val
+
 def test_check_symlog():
     """Test if the SymmetricalLogNormalizer passes the check"""
     x = [1,2,3,4,5]
     normal = SymmetricalLogNormalizer()
     assert numpy.isclose(normal.check_normalize_error(x),0)
+
+def test_check_zscore():
+    """Test if the ZScoreNormalizer passes the check"""
+    x = [1,2,3,4,5]
+    normal = ZScoreNormalizer()
+    normal.original_data = x
+    assert numpy.isclose(normal.check_normalize_error(x),0)
+
+def test_zscore_mean():
+    """Test if the ZScoreNormalizer returns the mean"""
+    x = [1,2,3,4,5]
+    normal = ZScoreNormalizer()
+    normal.original_data = x
+    assert normal.mean_val == 3
+
+def test_zscore_std():
+    """Test if the ZScoreNormalizer returns the standard deviation"""
+    x = [1,2,3,4,5]
+    normal = ZScoreNormalizer()
+    normal.original_data = x
+    assert normal.std_val == numpy.sqrt(2)
+
+def test_zscore_transform_error():
+    """Test if ZScoreNormalizer returns an error without original data"""
+    normal = ZScoreNormalizer()
+    with pytest.raises(ValueError):
+        normal.transform([1,2])
+    with pytest.raises(ValueError):
+        normal.inverse_transform([1,2]) 
+    with pytest.raises(ValueError):
+        normal.mean_val
+    with pytest.raises(ValueError):
+        normal.std_val
