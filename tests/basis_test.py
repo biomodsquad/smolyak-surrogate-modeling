4a26f1318faa065fa8369500ee4b88caecbce244
mzbush <mzb0108@auburn.edu>
2024-04-24 18:11:47 -0500

    Parameterize pytests

--- tests/basis_test.py
+++ tests/basis_test.py
@@ -42,6 +42,15 @@ def test_cheb_call_degree_0_1():
         assert f1(i) == i
 
 
+@pytest.mark.parametrize("n", list(range(20)))
+def test_cheb_call_random_points(n):
+    """Test chebyshev polynomial at some degree at some input"""
+    numpy.random.seed(567)
+    xs = numpy.random.rand(20) * 2 - 1
+    f = ChebyshevFirstKind(n)
+    assert numpy.allclose(f(xs), special.eval_chebyt(n, xs))
+
+
 def test_cheb_call_random_points():
     """Test chebyshev polynomial at some degree at some input"""
     numpy.random.seed(567)
@@ -72,56 +81,32 @@ def test_cheb_call_root_points():
     assert numpy.allclose(f(root_points), [0, 0, 0, 0])
 
 
-def test_cheb_call_random_points_multi_input():
+@pytest.mark.parametrize("n", list(range(20)))
+def test_cheb_call_random_points_multi_input(n):
     """Test that chebyshev polynomial call handles multiple x inputs"""
     numpy.random.seed(567)
-    xs = numpy.random.rand(20) * 2 - 1
-    f0 = ChebyshevFirstKind(0)
-    f1 = ChebyshevFirstKind(1)
-    fn = ChebyshevFirstKind(5)
-    assert numpy.allclose(f0(xs), special.eval_chebyt(0, xs))
-    assert numpy.allclose(f1(xs), special.eval_chebyt(1, xs))
-    assert numpy.allclose(fn(xs), special.eval_chebyt(5, xs))
+    xs = numpy.random.rand(20, 3, 4, 6) * 2 - 1
+    f = ChebyshevFirstKind(n)
+    answer = f(xs)
+    answer_check = special.eval_chebyt(n, xs)
+    assert answer.shape == answer_check.shape
+    assert numpy.allclose(answer, answer_check)
 
 
-def test_cheb_derivative():
+@pytest.mark.parametrize(
+    "n,answer",
+    [
+        (0, [0, 0]),
+        (1, [1, 1]),
+        (2, [4, -2]),
+    ],
+)
+def test_cheb_derivative(n, answer):
     """Test if the correct derivative is generated."""
-    f0 = ChebyshevFirstKind(0)
-    f1 = ChebyshevFirstKind(1)
-    f2 = ChebyshevFirstKind(2)
-    assert f0.derivative(1) == pytest.approx(0)
-    assert f1.derivative(1) == pytest.approx(1)
-    assert f2.derivative(1) == pytest.approx(4)
-
-    assert f0.derivative(-0.5) == pytest.approx(0)
-    assert f1.derivative(-0.5) == pytest.approx(1)
-    assert f2.derivative(-0.5) == pytest.approx(-2)
-
-    assert numpy.isclose(f0.derivative([1, -0.5]), [0, 0]).all()
-    assert numpy.isclose(f1.derivative([1, -0.5]), [1, 1]).all()
-    assert numpy.isclose(f2.derivative([1, -0.5]), [4, -2]).all()
-
-
-def test_cheb_2nd_derivative():
-    """Test if the correct derivative is generated."""
-    u0 = ChebyshevSecondKind(0)
-    u1 = ChebyshevSecondKind(1)
-    u2 = ChebyshevSecondKind(2)
-    assert u0.derivative(1) == pytest.approx(0)
-    assert u1.derivative(1) == pytest.approx(2)
-    assert u2.derivative(1) == pytest.approx(8)
-
-    assert u0.derivative(-1) == pytest.approx(0)
-    assert u1.derivative(-1) == pytest.approx(2)
-    assert u2.derivative(-1) == pytest.approx(-8)
-
-    assert u0.derivative(0.5) == pytest.approx(0)
-    assert u1.derivative(0.5) == pytest.approx(2)
-    assert u2.derivative(0.5) == pytest.approx(4)
-
-    assert numpy.isclose(u0.derivative([1, -1, 0.5]), [0, 0, 0]).all()
-    assert numpy.isclose(u1.derivative([1, -1, 0.5]), [2, 2, 2]).all()
-    assert numpy.isclose(u2.derivative([1, -1, 0.5]), [8, -8, 4]).all()
+    f = ChebyshevFirstKind(n)
+    assert f.derivative(1) == pytest.approx(answer[0])
+    assert f.derivative(-0.5) == pytest.approx(answer[1])
+    assert numpy.allclose(f.derivative([1, -0.5]), answer)
 
 
 def test_cheb_call_invalid_input():
@@ -174,13 +159,13 @@ def test_cheb_2nd_call_degree_0_1():
         assert f1(i) == i * 2
 
 
-def test_cheb_2nd_call_random_points():
+@pytest.mark.parametrize("n", list(range(20)))
+def test_cheb_2nd_call_random_points(n):
     """Test chebyshev polynomial at some degree at some input"""
     numpy.random.seed(567)
-    ns = numpy.random.randint(20, size=20)
     xs = numpy.random.rand(20) * 2 - 1
-    for n, x in zip(ns, xs):
-        f = ChebyshevSecondKind(n)
+    f = ChebyshevSecondKind(n)
+    for x in xs:
         assert numpy.isclose(f(x), special.eval_chebyu(n, x))
 
 
@@ -191,16 +176,33 @@ def test_cheb_2nd_call_root_points():
     assert numpy.allclose(f(root_points), [0, 0, 0])
 
 
-def test_cheb_2nd_call_random_points_multi_input():
+@pytest.mark.parametrize("n", list(range(20)))
+def test_cheb_2nd_call_random_points_multi_input(n):
     """Test that chebyshev polynomial call handles multiple x inputs"""
     numpy.random.seed(567)
-    xs = numpy.random.rand(20) * 2 - 1
-    f0 = ChebyshevSecondKind(0)
-    f1 = ChebyshevSecondKind(1)
-    fn = ChebyshevSecondKind(5)
-    assert numpy.allclose(f0(xs), special.eval_chebyu(0, xs))
-    assert numpy.allclose(f1(xs), special.eval_chebyu(1, xs))
-    assert numpy.allclose(fn(xs), special.eval_chebyu(5, xs))
+    xs = numpy.random.rand(20, 3, 4, 6) * 2 - 1
+    f = ChebyshevSecondKind(n)
+    answer = f(xs)
+    answer_check = special.eval_chebyu(n, xs)
+    assert answer.shape == answer_check.shape
+    assert numpy.allclose(answer, answer_check)
+
+
+@pytest.mark.parametrize(
+    "n,answer",
+    [
+        (0, [0, 0, 0]),
+        (1, [2, 2, 2]),
+        (2, [8, -8, 4]),
+    ],
+)
+def test_cheb_2nd_derivative(n, answer):
+    """Test if the correct derivative is generated."""
+    u = ChebyshevSecondKind(n)
+    assert u.derivative(1) == pytest.approx(answer[0])
+    assert u.derivative(-1) == pytest.approx(answer[1])
+    assert u.derivative(0.5) == pytest.approx(answer[2])
+    assert numpy.allclose(u.derivative([1, -1, 0.5]), answer)
 
 
 def test_cheb_2nd_call_invalid_input():
@@ -225,12 +227,22 @@ def test_cheb_2nd_derivative_invalid_input():
         f.derivative([0.5, 0.7, 3, 0.8])
 
 
-def test_trig_initial():
+@pytest.mark.parametrize(
+    "n,sigma",
+    [
+        (0, 0),
+        (2, -1),
+        (3, 2),
+        (7, 4),
+        (10, -5),
+    ],
+)
+def test_trig_initial(n, sigma):
     """test degrees returns correctly"""
-    f2 = Trigonometric(2)
-    assert f2.n == 2
+    f2 = Trigonometric(n)
+    assert f2.n == n
     assert numpy.array_equal(f2.domain, [0, 2 * numpy.pi])
-    assert f2.sigma == -1
+    assert f2.sigma == sigma
 
 
 def test_trig_ntype():
@@ -248,57 +260,54 @@ def test_trig_n_setter():
     assert f2.sigma == 2
 
 
-def test_trig_call():
+@pytest.mark.parametrize(
+    "n,expected",
+    [
+        (0, lambda x: numpy.exp(x * 0)),
+        (1, lambda x: numpy.exp(x * 1j)),
+        (2, lambda x: numpy.exp(x * 1j * -1)),
+    ],
+)
+def test_trig_call(n, expected):
     """Test call method of trigonometric basis function"""
-    f0 = Trigonometric(0)
-    f1 = Trigonometric(1)
-    f2 = Trigonometric(2)
-    f3 = Trigonometric(3)
+    f = Trigonometric(n)
     for i in [0, numpy.pi / 3, 3 * numpy.pi / 2]:
-        assert f0(i) == numpy.exp(i * 1j * 0)
-        assert f1(i) == numpy.exp(i * 1j)
-        assert f2(i) == numpy.exp(i * 1j * (-1))
-        assert f3(i) == numpy.exp(i * 1j * 2)
+        assert f(i) == expected(i)
 
 
-def test_trig_call_random_points_multi_input():
+@pytest.mark.parametrize(
+    "n,i",
+    [
+        (0, 0),
+        (1, 1j),
+        (5, 1j * 3),
+    ],
+)
+def test_trig_call_random_points_multi_input(n, i):
     """Test that Trigonometric polynomial call handles multiple x inputs"""
     numpy.random.seed(567)
-    xs = numpy.random.rand(20) * 2 * numpy.pi
-    f0 = Trigonometric(0)
-    f1 = Trigonometric(1)
-    fn = Trigonometric(5)
-    assert numpy.allclose(f0(xs), numpy.exp(xs * 1j * 0))
-    assert numpy.allclose(f1(xs), numpy.exp(xs * 1j))
-    assert numpy.allclose(fn(xs), numpy.exp(xs * 1j * 3))
+    xs = numpy.random.rand(20, 6, 3, 2) * 2 * numpy.pi
+    f = Trigonometric(n)
+    assert numpy.allclose(f(xs), numpy.exp(xs * i))
 
 
-def test_trig_derivative():
+@pytest.mark.parametrize(
+    "n,answer",
+    [
+        (0, [0, 0]),
+        (1, [1j * numpy.exp(1j), 1j * numpy.exp(numpy.pi * 1j * 1 / 6)]),
+        (
+            2,
+            [-1j * numpy.exp(1 * 1j * (-1)), -1j * numpy.exp(numpy.pi * 1j * (-1) / 6)],
+        ),
+    ],
+)
+def test_trig_derivative(n, answer):
     """Test if the correct derivative is generated."""
-    f0 = Trigonometric(0)
-    f1 = Trigonometric(1)
-    f2 = Trigonometric(2)
-    assert f0.derivative(1) == pytest.approx(0)
-    assert f1.derivative(1) == pytest.approx(1j * numpy.exp(1j))
-    assert f2.derivative(1) == pytest.approx(-1j * numpy.exp(1 * 1j * (-1)))
-
-    assert f0.derivative(numpy.pi / 6) == pytest.approx(0)
-    assert f1.derivative(numpy.pi / 6) == pytest.approx(
-        1j * numpy.exp(numpy.pi * 1j * 1 / 6)
-    )
-    assert f2.derivative(numpy.pi / 6) == pytest.approx(
-        -1j * numpy.exp(numpy.pi * 1j * (-1) / 6)
-    )
-
-    assert numpy.isclose(f0.derivative([1, numpy.pi / 6]), [0, 0]).all()
-    assert numpy.isclose(
-        f1.derivative([1, numpy.pi / 6]),
-        [1j * numpy.exp(1j), 1j * numpy.exp(numpy.pi * 1j * 1 / 6)],
-    ).all()
-    assert numpy.isclose(
-        f2.derivative([1, numpy.pi / 6]),
-        [-1j * numpy.exp(1 * 1j * (-1)), -1j * numpy.exp(numpy.pi * 1j * (-1) / 6)],
-    ).all()
+    f = Trigonometric(n)
+    assert f.derivative(1) == pytest.approx(answer[0])
+    assert f.derivative(numpy.pi / 6) == pytest.approx(answer[1])
+    assert numpy.allclose(f.derivative([1, numpy.pi / 6]), answer)
 
 
 def test_trig_call_invalid_input():
@@ -319,14 +328,6 @@ def test_trig_derivative_invalid_input():
         f.derivative(-0.04)
     with pytest.raises(ValueError):
         f.derivative(6.5)
-
-
-def test_trig_sigma():
-    """Test whether correct frequency is generated given a degree."""
-    f7 = Trigonometric(7)
-    f10 = Trigonometric(10)
-    assert f7.sigma == pytest.approx(4)
-    assert f10.sigma == pytest.approx(-5)
 
 
 def test_set_initialize_empty():
