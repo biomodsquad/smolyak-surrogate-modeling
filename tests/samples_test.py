e13ae8e675914f8d478b4b24d133488ed7ec4723
mzbush <mzb0108@auburn.edu>
2024-05-06 13:16:50 -0500

    Combine all point validation tests

--- tests/samples_test.py
+++ tests/samples_test.py
@@ -4,6 +4,127 @@ import numpy
 
 import smolyay.samples
 
+sample_points_answers = [
+    (smolyay.samples.ClenshawCurtisPointSet(0), [0]),
+    (smolyay.samples.ClenshawCurtisPointSet(1), [-1, 1]),
+    (smolyay.samples.ClenshawCurtisPointSet(2), [-1, 0, 1]),
+    (smolyay.samples.ClenshawCurtisPointSet(3), [-1, -0.5, 0.5, 1]),
+    (
+        smolyay.samples.ClenshawCurtisPointSet(8),
+        [
+            -1,
+            -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+            -1 / numpy.sqrt(2),
+            -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            0,
+            numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            1 / numpy.sqrt(2),
+            numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+            1,
+        ],
+    ),
+    (smolyay.samples.TrigonometricPointSet(0), [0]),
+    (smolyay.samples.TrigonometricPointSet(1), [0]),
+    (smolyay.samples.TrigonometricPointSet(2), [0, numpy.pi]),
+    (smolyay.samples.TrigonometricPointSet(3), [0, 2 * numpy.pi / 3, 4 * numpy.pi / 3]),
+    (
+        smolyay.samples.TrigonometricPointSet(9),
+        2 * numpy.pi * numpy.linspace(0, 8 / 9, 9),
+    ),
+    (smolyay.samples.NestedClenshawCurtisPointSet(0), [0]),
+    (smolyay.samples.NestedClenshawCurtisPointSet(1), [0, -1, 1]),
+    (
+        smolyay.samples.NestedClenshawCurtisPointSet(3),
+        [
+            0,
+            -1.0,
+            1.0,
+            -1 / numpy.sqrt(2),
+            1 / numpy.sqrt(2),
+            -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+            -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+        ],
+    ),
+    (smolyay.samples.SlowNestedClenshawCurtisPointSet(0), [0]),
+    (smolyay.samples.SlowNestedClenshawCurtisPointSet(1), [0, -1, 1]),
+    (
+        smolyay.samples.SlowNestedClenshawCurtisPointSet(3),
+        [
+            0,
+            -1.0,
+            1.0,
+            -1 / numpy.sqrt(2),
+            1 / numpy.sqrt(2),
+            -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+            -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+        ],
+    ),
+    (
+        smolyay.samples.SlowNestedClenshawCurtisPointSet(4),
+        [
+            0,
+            -1.0,
+            1.0,
+            -1 / numpy.sqrt(2),
+            1 / numpy.sqrt(2),
+            -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+            -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
+            numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
+        ],
+    ),
+    (smolyay.samples.NestedTrigonometricPointSet(0), [0]),
+    (
+        smolyay.samples.NestedTrigonometricPointSet(1),
+        [0, 2 * numpy.pi / 3, 4 * numpy.pi / 3],
+    ),
+    (
+        smolyay.samples.NestedTrigonometricPointSet(2),
+        2
+        * numpy.pi
+        * numpy.array(
+            [
+                0,
+                1 / 3,
+                2 / 3,
+                1 / 9,
+                2 / 9,
+                4 / 9,
+                5 / 9,
+                7 / 9,
+                8 / 9,
+            ]
+        ),
+    ),
+]
+
+sample_points_ids = [
+    "Non-nested Cheb [0]",
+    "Non-nested Cheb [1]",
+    "Non-nested Cheb [2]",
+    "Non-nested Cheb [3]",
+    "Non-nested Cheb [8]",
+    "Non-nested Trig [0]",
+    "Non-nested Trig [1]",
+    "Non-nested Trig [2]",
+    "Non-nested Trig [3]",
+    "Non-nested Trig [9]",
+    "Nested Cheb [0]",
+    "Nested Cheb [1]",
+    "Nested Cheb [3]",
+    "Nested Slow Cheb [0]",
+    "Nested Slow Cheb [1]",
+    "Nested Slow Cheb [3]",
+    "Nested Slow Cheb [4]",
+    "Nested Trig [0]",
+    "Nested Trig [1]",
+    "Nested Trig [2]",
+]
+
 
 def test_clenshaw_initial():
     """test default properties"""
@@ -12,34 +133,10 @@ def test_clenshaw_initial():
     assert f.degree == 7
 
 
-@pytest.mark.parametrize(
-    "degree,points",
-    [
-        (0, [0]),
-        (1, [-1, 1]),
-        (2, [-1, 0, 1]),
-        (3, [-1, -0.5, 0.5, 1]),
-        (
-            8,
-            [
-                -1,
-                -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-                -1 / numpy.sqrt(2),
-                -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                0,
-                numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                1 / numpy.sqrt(2),
-                numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-                1,
-            ],
-        ),
-    ],
-)
-def test_clenshaw_generate_values(degree, points):
-    """test the output of generate_points"""
-    assert numpy.allclose(
-        smolyay.samples.ClenshawCurtisPointSet(degree).points, points, atol=1e-10
-    )
+@pytest.mark.parametrize("object,points",sample_points_answers,ids=sample_points_ids)
+def test_generate_points(object, points):
+    """test the points of initialized UnidimensionalPointSet"""
+    assert numpy.allclose(object.points, points, atol=1e-10)
 
 
 def test_trig_initial():
@@ -47,26 +144,6 @@ def test_trig_initial():
     f = smolyay.samples.TrigonometricPointSet(7)
     assert numpy.array_equal(f.domain, [0, 2 * numpy.pi])
     assert f.frequency == 7
-
-
-@pytest.mark.parametrize(
-    "degree,points",
-    [
-        (0, [0]),
-        (1, [0]),
-        (2, [0, numpy.pi]),
-        (3, [0, 2 * numpy.pi / 3, 4 * numpy.pi / 3]),
-        (
-            9,
-            2 * numpy.pi * numpy.linspace(0, 8 / 9, 9),
-        ),
-    ],
-)
-def test_trig_generate_values(degree, points):
-    """test number of points is changed to 3"""
-    assert numpy.allclose(
-        smolyay.samples.TrigonometricPointSet(degree).points, points, atol=1e-10
-    )
 
 
 @pytest.mark.parametrize(
@@ -85,85 +162,6 @@ def test_nested_initial(nested_samples, domain):
     assert f._valid_cache == True
     f.points
     assert f._valid_cache == False
-
-
-@pytest.mark.parametrize(
-    "nested_samples,points",
-    [
-        (smolyay.samples.NestedClenshawCurtisPointSet(0), [0]),
-        (smolyay.samples.NestedClenshawCurtisPointSet(1), [0, -1, 1]),
-        (
-            smolyay.samples.NestedClenshawCurtisPointSet(3),
-            [
-                0,
-                -1.0,
-                1.0,
-                -1 / numpy.sqrt(2),
-                1 / numpy.sqrt(2),
-                -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-                -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-            ],
-        ),
-        (smolyay.samples.SlowNestedClenshawCurtisPointSet(0), [0]),
-        (smolyay.samples.SlowNestedClenshawCurtisPointSet(1), [0, -1, 1]),
-        (
-            smolyay.samples.SlowNestedClenshawCurtisPointSet(3),
-            [
-                0,
-                -1.0,
-                1.0,
-                -1 / numpy.sqrt(2),
-                1 / numpy.sqrt(2),
-                -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-                -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-            ],
-        ),
-        (
-            smolyay.samples.SlowNestedClenshawCurtisPointSet(4),
-            [
-                0,
-                -1.0,
-                1.0,
-                -1 / numpy.sqrt(2),
-                1 / numpy.sqrt(2),
-                -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-                -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-                numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-            ],
-        ),
-        (smolyay.samples.NestedTrigonometricPointSet(0), [0]),
-        (
-            smolyay.samples.NestedTrigonometricPointSet(1),
-            [0, 2 * numpy.pi / 3, 4 * numpy.pi / 3],
-        ),
-        (
-            smolyay.samples.NestedTrigonometricPointSet(2),
-            2
-            * numpy.pi
-            * numpy.array(
-                [
-                    0,
-                    1 / 3,
-                    2 / 3,
-                    1 / 9,
-                    2 / 9,
-                    4 / 9,
-                    5 / 9,
-                    7 / 9,
-                    8 / 9,
-                ]
-            ),
-        ),
-    ],
-)
-def test_nested_points(nested_samples, points):
-    """test number of points"""
-    assert numpy.allclose(nested_samples.points, points, atol=1e-10)
 
 
 @pytest.mark.parametrize(
@@ -186,6 +184,7 @@ def test_nested_num_points_per_level(nested_samples, num_points):
     """test number of points per level"""
     assert nested_samples.num_points == num_points
 
+
 @pytest.mark.parametrize(
     "nested_samples,start_level",
     [
@@ -204,7 +203,8 @@ def test_nested_num_points_per_level(nested_samples, num_points):
 )
 def test_nested_start_level(nested_samples, start_level):
     """test number of points per level"""
-    assert numpy.array_equal(nested_samples.start_level,start_level)
+    assert numpy.array_equal(nested_samples.start_level, start_level)
+
 
 @pytest.mark.parametrize(
     "nested_samples,end_level",
@@ -224,4 +224,4 @@ def test_nested_start_level(nested_samples, start_level):
 )
 def test_nested_end_level(nested_samples, end_level):
     """test number of points per level"""
-    assert numpy.array_equal(nested_samples.end_level,end_level)
+    assert numpy.array_equal(nested_samples.end_level, end_level)
