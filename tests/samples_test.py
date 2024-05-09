2450e856ed456e669329841a230898fe164f1a23
mzbush <mzb0108@auburn.edu>
2024-05-09 16:31:45 -0500

    Test domain error

--- tests/samples_test.py
+++ tests/samples_test.py
@@ -9,30 +9,12 @@ sample_points_answers = [
     (smolyay.samples.ClenshawCurtisPointSet([-1, 1], 1), [-1, 1]),
     (smolyay.samples.ClenshawCurtisPointSet([-1, 1], 2), [-1, 0, 1]),
     (smolyay.samples.ClenshawCurtisPointSet([-1, 1], 3), [-1, -0.5, 0.5, 1]),
-    (
-        smolyay.samples.ClenshawCurtisPointSet([-1, 1], 8),
-        [
-            -1,
-            -numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-            -1 / numpy.sqrt(2),
-            -numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-            0,
-            numpy.sqrt(numpy.sqrt(2) - 1) / (2**0.75),
-            1 / numpy.sqrt(2),
-            numpy.sqrt(numpy.sqrt(2) + 1) / (2**0.75),
-            1,
-        ],
-    ),
     (smolyay.samples.TrigonometricPointSet([0, 2 * numpy.pi], 0), [0]),
     (smolyay.samples.TrigonometricPointSet([0, 2 * numpy.pi], 1), [0]),
     (smolyay.samples.TrigonometricPointSet([0, 2 * numpy.pi], 2), [0, numpy.pi]),
     (
         smolyay.samples.TrigonometricPointSet([0, 2 * numpy.pi], 3),
         [0, 2 * numpy.pi / 3, 4 * numpy.pi / 3],
-    ),
-    (
-        smolyay.samples.TrigonometricPointSet([0, 2 * numpy.pi], 9),
-        2 * numpy.pi * numpy.linspace(0, 8 / 9, 9),
     ),
     (smolyay.samples.NestedClenshawCurtisPointSet([-1, 1], 0), [0]),
     (smolyay.samples.NestedClenshawCurtisPointSet([-1, 1], 1), [0, -1, 1]),
@@ -135,17 +117,23 @@ sample_points_answers = [
     ),
 ]
 
+all_sample_sets = [
+        smolyay.samples.ClenshawCurtisPointSet,
+        smolyay.samples.TrigonometricPointSet,
+        smolyay.samples.NestedClenshawCurtisPointSet,
+        smolyay.samples.SlowNestedClenshawCurtisPointSet,
+        smolyay.samples.NestedTrigonometricPointSet,
+]
+
 sample_points_ids = [
     "Non-nested Cheb [0]",
     "Non-nested Cheb [1]",
     "Non-nested Cheb [2]",
     "Non-nested Cheb [3]",
-    "Non-nested Cheb [8]",
     "Non-nested Trig [0]",
     "Non-nested Trig [1]",
     "Non-nested Trig [2]",
     "Non-nested Trig [3]",
-    "Non-nested Trig [9]",
     "Nested Cheb [0]",
     "Nested Cheb [1]",
     "Nested Cheb [2]",
@@ -212,13 +200,8 @@ def test_nested_initial(nested_samples):
 
 @pytest.mark.parametrize(
     "samples",
-    [
-        smolyay.samples.ClenshawCurtisPointSet,
-        smolyay.samples.TrigonometricPointSet,
-        smolyay.samples.NestedClenshawCurtisPointSet,
-        smolyay.samples.SlowNestedClenshawCurtisPointSet,
-        smolyay.samples.NestedTrigonometricPointSet,
-    ], ids=[
+    all_sample_sets,
+    ids=[
         "Unnested Cheb",
         "Unnested Trig",
         "Nested Cheb",
@@ -233,6 +216,23 @@ def test_set_domain(samples):
     f.domain = [-100, 100]
     assert f._valid_cache == False
     assert numpy.array_equal(f.domain, [-100, 100])
+
+@pytest.mark.parametrize(
+    "samples",
+    all_sample_sets,
+    ids=[
+        "Unnested Cheb",
+        "Unnested Trig",
+        "Nested Cheb",
+        "Nested Slow Cheb",
+        "Nested Trig"],
+)
+def test_domain_error(samples):
+    """test default properties"""
+    with pytest.raises(TypeError):
+        samples([-10, 5, 10],7)
+    with pytest.raises(ValueError):
+        samples([10, -10],7)
 
 @pytest.mark.parametrize("samples,points", sample_points_answers, ids=sample_points_ids)
 def test_generate_points(samples, points):
